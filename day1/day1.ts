566c96517afe8cc52f279e85ccb24dc1e65607b5
Matt Perry <forloop616@gmail.com>
2023-11-30 23:37:25 -0500

    initial commit

---
+++ day1/day1.ts
@@ -0,0 +1,12 @@
+import { readInputLines } from '../shared/utils';
+
+const day1 = (lines: string[]): number => {
+    return 0;
+}
+
+(async () => {
+    const input = await readInputLines('day1');
+    const part1 = day1(input);
+
+    console.log(part1);
+})();
